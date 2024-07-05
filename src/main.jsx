c4441ba143a14a35773b286091b7d7336b609cbd
Chuthamat Naktrakul <chuthamatnaktrakul@MacBook-Air-khxng-Chuthamat.local>
2024-07-06 02:32:36 +0700

    edit import main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import "~@flaticon/flaticon-uicons/css/all/all";
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
